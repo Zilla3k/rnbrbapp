e0062a90b72f18e4600dc09ff988091b39faad43
henrique <henriquepelanda.web@gmail.com>
2025-05-28 19:11:28 -0300

    feat: profile screen

--- src/app/(tabs)/schedule.tsx
+++ src/app/(tabs)/schedule.tsx
@@ -13,20 +13,20 @@ export default function Appointments() {
   const [show, setShow] = useState(false);
   const [selectedTime, setSelectedTime] = useState<string | null>(null);
   const availableTimes = {
-    morning: [
+    Manhã: [
       '08:00', '08:30',
       '09:00', '09:30',
       '10:00', '10:30',
       '11:00', '11:30',
       '12:00',
     ],
-    afternoon: [
+    Tarde: [
       '14:00', '14:30',
       '15:00', '15:30', 
       '16:00', '16:30', 
       '17:00', '17:30', 
     ],
-    evening: [
+    Noite: [
       '18:00', '18:30', 
       '19:00', '19:30',
       '20:00', '20:30', 
@@ -74,14 +74,6 @@ export default function Appointments() {
 
       <ScrollView showsVerticalScrollIndicator={false}>
         <Text style={styles.scrollViewTitle}>Horários</Text>
-        {/* 
-        <AppointmentsCustom
-          title='Manha'
-          availableTimes={availableTimes.morning}
-          handleTimeSelection={handleTimeSelection}
-          selectedTime={selectedTime}
-        />
-        */}
 
         {Object.entries(availableTimes).map(([key, times]) => (
           <AppointmentsCustom
